cdbf133c6b908f5ee80a2b30d385434dcdf4f3fa
Peter Bankole <peter.t.bankole@gmail.com>
2025-05-25 06:00:37 -0700

    Added Jquery for  post btn in mobile position

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   
+    //Post Dispears when it reaches footer on phone screens
+    $(window).scroll(function(){
+          
+        if ($(this).scrollTop() < 4100) {
+            $('#mobile-post-btn').fadeIn();
+        } else {
+            $('#mobile-post-btn').fadeOut();
+        }
+    })
+})
